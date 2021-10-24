bea03354ca9730432f6a78ad3ab62534e1bc54e6
Ananyamadusudan <37510119+Ananyamadusudan@users.noreply.github.com>
2021-10-24 17:00:16 +0530

    Removed commented code

--- TopoSort.java
+++ TopoSort.java
@@ -1,8 +1,5 @@
 package com.company;
 
-
-import apple.laf.JRSUIUtils;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -10,29 +7,17 @@ import java.util.List;
 import java.util.Set;
 import java.util.Stack;
 
-public class Main {
+public class TopoSort {
 
     public static void main(String[] args) {
 	// write your code here
-      /*int[][] graph = {{1,2},{0}};
+      int[][] graph = {{1,2},{0}};
       int[] result = getCompilationOrder(graph, 3);
       for (int i : result) {
         System.out.println(i);
-      }*/
+      }
 
-        Tree.Node node = new Tree.Node(1);
-        Tree.Node node1 = new Tree.Node(3);
-      Tree.Node node3 = new Tree.Node(4);
-      Tree.Node node4 = new Tree.Node(6);
-      Tree.Node node5 = new Tree.Node(7);
-      Tree.Node node6 = new Tree.Node(8);
-
-      node1.setLeft(node4);
-      node1.setRight(node5);
-      node3.setLeft(node6);
-      node.setLeft(node1);
-      node.setRight(node3);
-      Tree.spiralTraversal(node);
+     
 
     }
     static int[] getCompilationOrder(int[][] graph, int V) {
